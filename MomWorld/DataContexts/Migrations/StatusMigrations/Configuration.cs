a1eafc5dc37dc7b17135ea1fa1068fbb934c6fb2
meobeoi <trungnm0512@gmail.com>
2015-03-29 17:08:52 +0700

    Create Status Module

---
+++ MomWorld/DataContexts/Migrations/StatusMigrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace MomWorld.DataContexts.Migrations.StatusMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MomWorld.DataContexts.StatusDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DataContexts\Migrations\StatusMigrations";
+        }
+
+        protected override void Seed(MomWorld.DataContexts.StatusDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
