11d20c011af1ca8848d974114999af4ee9566265
meobeoi <trungnm0512@gmail.com>
2015-03-29 12:29:27 +0700

    Update User Profile

---
+++ Articles.Entities/Diary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomWorld.Entities
+{
+    class Diary
+    {
+        public Diary()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        [MaxLength(128)]
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+        public string time { get; set; }
+    }
+}
