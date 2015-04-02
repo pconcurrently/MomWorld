4dbb1f1cf5f8558a8f5e92c26f639e62ea6779e3
shortgiraffe4 <shortgiraffe4@gmail.com>
2015-04-02 21:54:30 +0700

    Search module

--- MomWorld/Controllers/SearchController.cs
+++ MomWorld/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MomWorld.DataContexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,22 @@ namespace MomWorld.Controllers
 {
     public class SearchController : Controller
     {
+        private ArticleDb db = new ArticleDb();
         // GET: Search
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetResults(string id)
+        {
+            if (id == null || id == string.Empty)
+            {
+                return Json(string.Empty);
+            }
+
+            var arts = db.Articles.ToList().FindAll(a => a.Content.Contains(id));
+            return Json(arts, JsonRequestBehavior.AllowGet);
         }
     }
 }
