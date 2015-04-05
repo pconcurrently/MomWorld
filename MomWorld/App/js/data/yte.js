ae1b395a09bfed0fb4466afc95eb73add66dd868
meobeoi <trungnm0512@gmail.com>
2015-04-05 10:22:32 +0700

    Update Quiz Function
    Related Work Items: #2

--- MomWorld/App/js/data/yte.js
+++ MomWorld/App/js/data/yte.js
@@ -2,7 +2,7 @@
     "quiz": {
         "Id": 3,
         "name": "Y TE",
-        "description": "C# and .Net Quiz (contains C#, .Net Framework, Linq, etc.)"
+        "description": "Y te ......"
     },
     "config":{
         "shuffleQuestions": true,
@@ -11,25 +11,25 @@
         "autoMove": true
     },
     "questions": [{
-        "Id": 2010,
-        "Name": "Which of the following assemblies can be stored in Global Assembly Cache?", 
+        "Id": 32010,
+        "Name": "Bac sy mac ao mau gi", 
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 10552, "QuestionId": 1010, "Name": "Private Assemblies", "IsAnswer": false },
-            { "Id": 10562, "QuestionId": 1010, "Name": "Friend Assemblies", "IsAnswer": false },
-            { "Id": 10572, "QuestionId": 1010, "Name": "Public Assemblies", "IsAnswer": false },
-            { "Id": 10582, "QuestionId": 1010, "Name": "Shared true", "IsAnswer": true }],
+            { "Id": 310553, "QuestionId": 32010, "Name": "Hong", "IsAnswer": false },
+            { "Id": 310562, "QuestionId": 32010, "Name": "Xanh", "IsAnswer": false },
+            { "Id": 310572, "QuestionId": 32010, "Name": "Do", "IsAnswer": false },
+            { "Id": 310582, "QuestionId": 32010, "Name": "True", "IsAnswer": true }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
-        "Id": 2019,
-        "Name": "Which of the following does NOT represent Integer?",
+        "Id": 32019,
+        "Name": "Benh vien co mau tang",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "true", "IsAnswer": true },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Byte", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Short", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "Long", "IsAnswer": false }],
+            { "Id": 31055, "QuestionId": 32019, "Name": "True", "IsAnswer": true },
+            { "Id": 31056, "QuestionId": 32019, "Name": "1", "IsAnswer": false },
+            { "Id": 31057, "QuestionId": 32019, "Name": "2", "IsAnswer": false },
+            { "Id": 31058, "QuestionId": 32019, "Name": "3", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     }]
 }
