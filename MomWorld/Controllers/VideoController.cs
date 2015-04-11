92ea86109f79713070bea49e7f56f2fbafb670c8
khoahnse90056 <khoahnse90056@gmail.com>
2015-04-11 10:24:09 +0700

    Video with layout

--- MomWorld/Controllers/VideoController.cs
+++ MomWorld/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MomWorld.DataContexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace MomWorld.Controllers
 {
     public class VideoController : Controller
     {
+        private IdentityDb identityDb = new IdentityDb();
         // GET: Video
         public ActionResult Index()
         {
+            ViewBag.CurrentUser = identityDb.Users.FirstOrDefault(u=>u.UserName.Equals(User.Identity.Name));
             return View();
         }
     }
