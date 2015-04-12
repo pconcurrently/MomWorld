dd57f96883ade5bc476bfd3d4df48293c568c81b
khoahnse90056 <khoahnse90056@gmail.com>
2015-04-12 15:48:59 +0700

    layout

--- MomWorld/Controllers/ChatController.cs
+++ MomWorld/Controllers/ChatController.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using MomWorld.DataContexts;
+using MomWorld.Entities;
+using MomWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace MomWorld.Controllers
 {
     public class ChatController : Controller
     {
+
+        private IdentityDb identityDb = new IdentityDb();
         // GET: Chat
         public ActionResult Index(string username)
         {
+            ViewBag.CurrentUser = identityDb.Users.FirstOrDefault(u => u.UserName.Equals(User.Identity.Name));
             ViewData["ChatUsername"] = username;
             return View();
         }
